7e42826a6afa68fdcd39a6ab9804741d67f73d5d
Leicunn Studio <leicunnstudio@users.noreply.github.com>
2018-07-18 21:30:20 +0300

    watermark works, compression DOESN'T

--- src/optimise/optimisation.java
+++ src/optimise/optimisation.java
@@ -1,24 +1,63 @@
 package optimise;
 
+//import org.xerial.snappy.Snappy;
+
 import messages.errorSuccess;
 
 import java.awt.*;
 import java.io.*;
 import java.awt.image.*;
+import java.net.URISyntaxException;
 import javax.imageio.*;
+import javax.imageio.stream.FileImageOutputStream;
+import javax.imageio.stream.ImageInputStream;
 import javax.imageio.stream.ImageOutputStream;
+//import javax.imageio.stream.ImageOutputStream;
+
+import java.io.IOException;
+import java.io.File;
+import java.lang.Object;
+import java.util.Iterator;
 
 public class optimisation {
 
     File watermark;
 
-    public void compress(String path, String new_name) {
+    public void compress(String path, String newName) {
+
+
+//        public class HuffmanEncodingTest extends TestCase {
+
+//        public boolean isSameFile(fileCharIterator it, fileCharIterator it2) {
+//            while (it.hasNext() && it2.hasNext()) {
+//                String char1 = it.next();
+//                String char2 = it2.next();
+//
+//                if (!char1.equals(char2))
+//                    return false;
+//            }
+//            return true;
+//        }
+
+
+//        public void testImageFile() {
+            // encode
+//            huffmanEncoding.encode(path, newName, 0);
+
+            // decode
+//            huffmanEncoding.decode("HangInThere.jpg.huffman", "myHangInThere.jpg");
+
+            // check if these two files are the same
+//            boolean equals = isSameFile(new fileCharIterator("myHangInThere.jpg"),
+//                    new fileCharIterator("HangInThere.jpg"));
+//            assertEquals(true, equals);
+//        }
 
         try {
             File input = new File(path);
             BufferedImage image = ImageIO.read(input);
 
-            File output = new File(new_name);
+            File output = new File(newName);
             OutputStream out = new FileOutputStream(output);
 
             ImageWriter writer =  ImageIO.getImageWritersByFormatName("jpg").next();
@@ -27,8 +66,9 @@ public class optimisation {
 
             ImageWriteParam param = writer.getDefaultWriteParam();
             if (param.canWriteCompressed()){
-                param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
-                param.setCompressionQuality(1.0f);
+                param.setCompressionMode(ImageWriteParam.MODE_COPY_FROM_METADATA);
+                param.setCompressionType("LZW");
+                param.setCompressionQuality(1f);
             }
 
             writer.write(null, new IIOImage(image, null, null), param);
@@ -42,12 +82,80 @@ public class optimisation {
             errorSuccess errorSuccessObj = new errorSuccess();
             errorSuccessObj.successMessage();
         }
+
+
+
+        // String fileName = "4958813_1";
+//        String fileName = "4848970_1";
+//        String inFileType = ".PNG";
+//        String outFileType = ".TIFF";
+
+//        try {
+//            ImageIO.scanForPlugins();
+//
+//            File fInputFile = new File(path);
+//            InputStream fis = new BufferedInputStream(new FileInputStream(
+//                    fInputFile));
+//            PNGImageReaderSpi spi = new PNGImageReaderSpi();
+//            ImageReader reader = spi.createReaderInstance();
+//
+//            ImageInputStream iis = ImageIO.createImageInputStream(fis);
+//            reader.setInput(iis, true);
+//            BufferedImage bi = reader.read(0);
+//
+//            TIFFImageWriterSpi tiffspi = new TIFFImageWriterSpi();
+//            ImageWriter writer = tiffspi.createWriterInstance();
+            //Iterator<ImageWriter> iter =  ImageIO.getImageWritersByFormatName("TIFF");
+            //ImageWriter writer = iter.next();
+//
+//            ImageWriteParam param = writer.getDefaultWriteParam();
+//            param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+//
+//            param.setCompressionType("LZW");
+//            param.setCompressionQuality(1f);
+//            File fOutputFile = new File(newName);
+//            ImageOutputStream ios = ImageIO.createImageOutputStream(fOutputFile);
+//            writer.setOutput(ios);
+//            writer.write(bi);
+//            writer.write(null, new IIOImage(bi, null, null), param);
+//        } catch (IOException ioex) {
+//            System.err.println(ioex);
+//        }
+
+//        ImageIO.scanForPlugins();
+//
+//        File fInputFile = new File(path);
+//        InputStream fis = new BufferedInputStream(new FileInputStream(
+//                fInputFile));
+//        PNGImageReaderSpi spi = new PNGImageReaderSpi();
+//        ImageReader reader = spi.createReaderInstance();
+//
+//        ImageInputStream iis = ImageIO.createImageInputStream(fis);
+//        reader.setInput(iis, true);
+//        BufferedImage bi = reader.read(0);
+//
+//        TIFFImageWriterSpi tiffspi = new TIFFImageWriterSpi();
+//        ImageWriter writer = tiffspi.createWriterInstance();
+//        //Iterator<ImageWriter> iter =  ImageIO.getImageWritersByFormatName("TIFF");
+//        //ImageWriter writer = iter.next();
+//
+//        ImageWriteParam param = writer.getDefaultWriteParam();
+//        param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+//
+//        param.setCompressionType("LZW");
+//        param.setCompressionQuality(0.5f);
+//        File fOutputFile = new File(newName);
+//        ImageOutputStream ios = ImageIO.createImageOutputStream(fOutputFile);
+//        writer.setOutput(ios);
+//        writer.write(bi);
+
+
+
     }
 
     public void watermark(String path, String newName) {
         try {
-//            watermark = new File("../media/watermark.jpg");
-            watermark = new File("/home/wangui/Classwork/Multimedia Systems and Applications/Project/optimage/src/media/watermark.jpg");
+            watermark = new File(getClass().getResource("../media/watermark.jpg").toURI());
             File fileToWatermark = new File(path);
             BufferedImage imageToWatermark = ImageIO.read(fileToWatermark);
             BufferedImage watermarkImage = ImageIO.read(watermark);
@@ -64,16 +172,20 @@ public class optimisation {
             // paints the image watermark
             g2d.drawImage(watermarkImage, topLeftX, topLeftY, null);
 
-//            ImageIO.write(imageToWatermark, "jpg", destImageFile);
             File newImagePath = new File(newName);
             ImageIO.write(imageToWatermark, "jpg", newImagePath);
-//            ImageIO.write(newImagePath);
             g2d.dispose();
 
             System.out.println("The image watermark is added to the image.");
 
         } catch (IOException ex) {
             System.err.println(ex);
+        } catch (URISyntaxException synex) {
+            System.err.println(synex);
+        }
+        finally {
+            errorSuccess errorSuccessObj = new errorSuccess();
+            errorSuccessObj.successMessage();
         }
     }
 }
